26d82a6bc7d981a0d389e9c33efb4e6810f0fea1
Sam Hashemi <sam@samhashemi.com>
2014-06-25 16:41:17 +0200

    Enable NORTA.

--- config.js
+++ config.js
@@ -3,6 +3,17 @@ module.exports = {
   , agencies: [
       /* Put agency_key names from gtfs-data-exchange.com.  
       Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com */
+    // 'san-francisco-municipal-transportation-agency',
+    // 'ac-transit',
+    // 'city-of-seattle',
+    // 'metropolitan-atlanta-rapid-transit-authority',
+    { agency_key: 'norta', url: 'http://samhashemi.com/20140629G.clean.zip'}
+    // 'la-metro',
+    // 'wmata',
+    // 'mta-bus-company',
+    // 'carta',
+    // 'massachusetts-bay-transportation-authority',
+    // 'detroit-department-of-transportation',
     //   'alamedaoakland-ferry'
     // , { agency_key: 'caltrain', url: 'http://www.gtfs-data-exchange.com/agency/caltrain/latest.zip'}
     // , 'ac-transit'
@@ -10,15 +21,5 @@ module.exports = {
     // , 'san-francisco-municipal-transportation-agency'
     // , 'bay-area-rapid-transit'
     // , 'golden-gate-ferry'
-    'san-francisco-municipal-transportation-agency',
-    'ac-transit',
-    // 'la-metro',
-    'city-of-seattle',
-    // 'wmata',
-    'metropolitan-atlanta-rapid-transit-authority',
-    // 'mta-bus-company',
-    // 'carta',
-    // 'massachusetts-bay-transportation-authority',
-    // 'detroit-department-of-transportation',
     ]
-}
+};
